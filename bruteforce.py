731ed1cdfecf3369d8f05d8a9d9c4ffd4fc7de9b
Cyril Leblanc <cyril.leblanc@outlook.com>
2024-12-30 22:51:17 +0100

    bruteforce solution

--- bruteforce.py
+++ bruteforce.py
@@ -1,69 +1,27 @@
-import csv
-import datetime
-
-
-def read_csv(file_name, stock_column, price_column, profit_column):
-    with open(file_name, mode="r") as file:
-        reader = csv.DictReader(file, delimiter=",")
-
-        data = []
-
-        for row in reader:
-            stock = row[stock_column]
-            price = float(row[price_column])
-            profit = float(row[profit_column].replace("%", ""))
-            if profit >= 1:
-                profit /= 100
-
-            if price > 0 and profit >= 0:
-                data.append({"stock": stock, "price": price, "profit": profit})
-
-        return data
+from itertools import combinations
 
 
 def find_best_stock(data, budget):
-    for action in data:
-        action["ratio"] = action["profit"] / action["price"]
+    best_profit = 0
+    best_combination = []
 
-    selected_actions = []
-    total_cost = 0
-    total_profit = 0
+    for i in range(1, len(data) + 1):
+        all_combinations = combinations(data, i)
 
-    sorted_data = sorted(data, key=lambda x: x["ratio"], reverse=True)
-    for action in sorted_data:
-        cost = action["price"]
-        if budget >= cost:
-            selected_actions.append(action["stock"])
-            budget -= cost
-            total_cost += cost
-            total_profit += cost * action["profit"]
+        for combination in all_combinations:
+            total_cost = sum(stock["price"] for stock in combination)
+            total_profit = sum(
+                stock["price"] * stock["profit"] for stock in combination
+            )
 
-    return selected_actions, round(total_cost, 2), round(total_profit, 2)
+            if total_cost <= budget and total_profit > best_profit:
+                best_profit = total_profit
+                best_combination = combination
 
-
-# "Actions #"
-# "Coût par action (en euros)"
-# "Bénéfice (après 2 ans)"
-def main():
-    start_time = datetime.datetime.now()
-
-    file_name = "dataset3.csv"
-    stock_column = "name"
-    price_column = "price"
-    profit_column = "profit"
-
-    data = read_csv(file_name, stock_column, price_column, profit_column)
-    budget = 500
-
-    selected_actions, total_cost, total_profit = find_best_stock(data, budget)
-
-    print(f"Actions choisies: {selected_actions}")
-    print(f"Coût total: {total_cost}€")
-    print(f"Profit total (après 2 ans): {total_profit}€")
-
-    end_time = datetime.datetime.now()
-    print(end_time - start_time)
-
-
-if __name__ == "__main__":
-    main()
+    selected_stocks = [stock["stock"] for stock in best_combination]
+    total_cost = sum(stock["price"] for stock in best_combination)
+    return (
+        selected_stocks,
+        round(total_cost, 2),
+        round(best_profit, 2),
+    )
